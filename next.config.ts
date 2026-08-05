4071d5f8a0ec788980bddce5fc8c69abce68414b
milan <milan.stvrtecky@gmail.com>
2026-08-05 10:18:27 +0200

    next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
         hostname: "pub-82020269fabb4c89ac7416178b29bf31.r2.dev",
-        port: "",
         pathname: "/**",
       },
     ],
