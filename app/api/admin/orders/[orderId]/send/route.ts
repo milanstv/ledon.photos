01e059f31da0331d68099e07c33369f0887e579b
milan <milan.stvrtecky@gmail.com>
2026-08-14 00:14:08 +0200

    Fix send order escape handling

--- app/api/admin/orders/[orderId]/send/route.ts
+++ app/api/admin/orders/[orderId]/send/route.ts
@@ -100,8 +100,8 @@ function createTokenHash(token: string) {
     .digest("hex");
 }
 
-function escapeHtml(value: string) {
-  return value
+function escapeHtml(value: unknown) {
+  return String(value ?? "")
     .replaceAll("&", "&amp;")
     .replaceAll("<", "&lt;")
     .replaceAll(">", "&gt;")
