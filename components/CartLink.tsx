22bf9c1fedf4c0e5c6db08afea21ba58d9d439d1
milan <milan.stvrtecky@gmail.com>
2026-08-31 22:11:06 +0200

    components/CartLink.tsx

--- components/CartLink.tsx
+++ components/CartLink.tsx
@@ -16,13 +16,13 @@ export default function CartLink({
   return (
     <Link
       href="/cart"
-      className={`${className ?? ""} inline-flex items-center gap-2 text-lg font-semibold`}
+      className={`${className ?? ""} inline-flex items-center gap-3 text-2xl font-bold`}
     >
       <span>Košík</span>
 
-      <span className="inline-flex min-w-7 h-7 items-center justify-center rounded-full border border-white/40 px-2 text-sm">
-        {count}
-      </span>
+      <span className="inline-flex min-w-10 h-10 items-center justify-center rounded-full border-2 border-white/60 px-3 text-xl font-bold">
+       {count}
+       </span>
     </Link>
   );
 }
